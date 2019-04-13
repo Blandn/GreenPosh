65da7b838a3547c0e5c44fadcf80b41693693368
Nate Bland <Blandn@wit.edu>
2019-04-13 14:44:21 -0400

    initial commit

--- eclipse-newspace(VaccineProgram)/VaccineProgram/src/application/Main.java
+++ eclipse-newspace(VaccineProgram)/VaccineProgram/src/application/Main.java
@@ -1,33 +1,18 @@
 package application;
 
-import illnessClasses.AsepticMeningitis;
-import illnessClasses.Asthma;
-import illnessClasses.Campylobacter;
-import illnessClasses.CerebellarAtaxia;
-import illnessClasses.Croup;
-import illnessClasses.EColi;
-import illnessClasses.Encephalitis;
-import illnessClasses.FoodAllergies;
-import illnessClasses.FoodPoisoning;
-import illnessClasses.Measles;
-import illnessClasses.Mumps;
-import illnessClasses.Pneumonia;
-import illnessClasses.Rotavirus;
-import illnessClasses.Salmonella;
-import illnessClasses.Sepsis;
-import illnessClasses.StomachBug;
-import illnessClasses.Tonsillitis;
-import illnessClasses.WhoopingCough;
-	
+import java.io.IOException;
 import java.net.URL;
 import java.util.Random;
 import java.util.ResourceBundle;
+
+import com.sun.glass.ui.Window.Level;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
@@ -44,19 +29,23 @@ public class Main extends Application implements Initializable {
 
 	@FXML
 	Text myText;
+	
+	@FXML  
+	Button homeButton;
    
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		final FXMLLoader loader = new FXMLLoader(getClass().getResource("GUI1.fxml"));
-		final Pane p = loader.load();
+		 FXMLLoader loader = new FXMLLoader(getClass().getResource("GUI1.fxml"));
+		 Pane p = loader.load();
 	        
 		primaryStage.setScene(new Scene(p));
 		primaryStage.show();
 	}
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-	}
 	
+	}
+		
 	public static void main(String[] args) {
 		launch(args);
 		}
